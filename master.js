cd4fedc35f406930061dccb2132aebc99cf74e9f
Neitsab <bastienthefly@live.fr>
2017-01-18 10:33:54 +0100

    Modification page Index et master.js
    Véritable caroussel fait main sur javascript, bisous.

--- master.js
+++ master.js
@@ -1,4 +1,3 @@
-
 $(function(){
 
   var container = $('#wrap');
@@ -28,5 +27,31 @@ $(function(){
     formSign.hide();
     formLog.hide();
   });
-});
 
+// Faire défiler des images en background
+
+var images = [
+  "images/taxi1.jpg",
+  "images/taxi4.jpg",
+  "images/taxi5.jpg"
+];
+var $body = $("body"),
+    $bg = $(".back"),
+    n = images.length,
+    c = 0; // Loop Counter
+
+// Preload Array of images...
+for(var i=0; i<n; i++){
+  var tImg = new Image();
+  tImg.src = images[i];
+}
+
+$body.css({backgroundImage : "url("+images[c]+")"});
+
+(function loopBg(){
+  $bg.hide().css({backgroundImage : "url("+images[++c%n]+")"}).delay(3000).fadeTo(2000, 1, function(){
+    $body.css({backgroundImage : "url("+images[c%n]+")"});
+    loopBg();
+  });
+}());
+});
